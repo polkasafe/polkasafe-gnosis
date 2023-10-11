f324aad23b16800fc1e592f977ab5100483c0fc5
AleemAlam <aleem.alam@outlook.com>
2023-10-11 20:42:58 +0530

    fix-link-multisig

--- src/components/Multisig/LinkMultisig/LinkMultisig.tsx
+++ src/components/Multisig/LinkMultisig/LinkMultisig.tsx
@@ -31,7 +31,7 @@ const LinkMultisig = ({ onCancel }: { onCancel: () => void }) => {
 	const [nameAddress, setNameAddress] = useState(true);
 	const [viewOwners, setViewOwners] = useState(true);
 	const [viewReviews, setViewReviews] = useState(true);
-	const { address, addressBook, gnosisSafe } = useGlobalUserDetailsContext();
+	const { address, addressBook, gnosisSafe, setUserDetailsContextState } = useGlobalUserDetailsContext();
 	const { network } = useGlobalApiContext();
 	const navigate = useNavigate();
 
@@ -113,7 +113,7 @@ const LinkMultisig = ({ onCancel }: { onCancel: () => void }) => {
 		if (multisigData) {
 			try {
 
-				await fetch(`${FIREBASE_FUNCTIONS_URL}/createMultisigEth`, {
+				const data = await fetch(`${FIREBASE_FUNCTIONS_URL}/createMultisigEth`, {
 					body: JSON.stringify({
 						signatories: signatoriesArray.map(item => item.address),
 						threshold,
@@ -124,11 +124,19 @@ const LinkMultisig = ({ onCancel }: { onCancel: () => void }) => {
 					method: 'POST'
 				});
 
+				const linkedMultisigData = await data.json();
+
+				setUserDetailsContextState(prev => ({
+					...prev,
+					multisigAddresses:[...prev.multisigAddresses, linkedMultisigData]
+				}));
+
 				queueNotification({
 					header: 'Success!',
 					message: 'Multisig Linked Successfully.',
 					status: NotificationStatus.SUCCESS
 				});
+
 				navigate('/', { replace: true });
 			} catch (err) {
 				console.log(err);
